d2f4b2b23633a2195afab98627fea520af9155f6
Nico Zhu <v-zhumin@microsoft.com>
2017-11-16 18:27:18 +0800

    Add project files.

---
+++ CustomLabelTest/CustomLabelTest.UWP/CustomLabelRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+namespace CustomLabelTest.UWP
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            HtmlTextBehavior behavior = new HtmlTextBehavior();
+          
+            
+        }
+    }
+}
